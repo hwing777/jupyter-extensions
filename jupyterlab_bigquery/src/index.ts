69bb9a1885e01cd12774fbd14f28aadaa84984e3
hwing777 <hwing777@gmail.com>
2020-06-29 18:37:00 +0000

    Add WidgetTracker

--- jupyterlab_bigquery/src/index.ts
+++ jupyterlab_bigquery/src/index.ts
@@ -8,13 +8,15 @@ import {
 
 import { ListItemsWidget } from './components/list_items_panel/list_tree_item_widget';
 import { ListProjectsService } from './components/list_items_panel/service/list_items';
+import { IBigQueryFactory } from './tokens';
 import { WidgetManager } from './utils/widget_manager';
 import { Clipboard } from '@jupyterlab/apputils';
 
 const copyID = 'bigquery:copy-id';
 
-function addCommands(app: JupyterFrontEnd) {
+function addCommands(app: JupyterFrontEnd, factory: IBigQueryFactory) {
   const { commands } = app;
+  const { tracker } = factory;
 
   commands.addCommand(copyID, {
     label: 'Copy ID',
@@ -22,20 +24,27 @@ function addCommands(app: JupyterFrontEnd) {
     isVisible: () => true,
     iconClass: 'jp-MaterialIcon jp-CopyIcon',
     execute: () => {
-      console.log('Its working!');
-      Clipboard.copyToSystem('hello :0');
+      const widget = tracker.currentWidget;
+      if (!widget) {
+        return;
+      }
+
+      const id = widget.id;
+
+      console.log(id);
+      Clipboard.copyToSystem(id);
     },
   });
 }
 
-async function activate(app: JupyterFrontEnd) {
+async function activate(app: JupyterFrontEnd, factory: IBigQueryFactory) {
   const manager = new WidgetManager(app);
   const context = { app: app, manager: manager };
   const listProjectsService = new ListProjectsService();
   const listWidget = new ListItemsWidget(listProjectsService, context);
   listWidget.addClass('jp-BigQueryIcon');
 
-  addCommands(app);
+  addCommands(app, factory);
   app.contextMenu.addItem({
     command: copyID,
     selector: '.jp-bigquery-DirListing',
