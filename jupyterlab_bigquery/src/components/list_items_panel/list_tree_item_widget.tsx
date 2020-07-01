024493694aa6f0814d1248feed33178f6cd89866
hwing777 <hwing777@gmail.com>
2020-07-01 17:07:42 +0000

    Fix context for launching details widget

--- jupyterlab_bigquery/src/components/list_items_panel/list_tree_item_widget.tsx
+++ jupyterlab_bigquery/src/components/list_items_panel/list_tree_item_widget.tsx
@@ -20,25 +20,15 @@ export default class ListItemsWidget extends ReduxReactWidget {
   id = 'listitems';
   // private _items: Contents.IModel[] = [];
   private visibleSignal = new Signal<ListItemsWidget, boolean>(this);
-  private listProjectsService = new ListProjectsService();
 
   constructor(
-    // private readonly listProjectsService: ListProjectsService,
+    private readonly listProjectsService: ListProjectsService,
     private context: Context
   ) {
     super();
     this.title.iconClass = 'jp-Icon jp-Icon-20 jp-BigQueryIcon';
     this.title.caption = 'BigQuery In Notebooks';
   }
-
-  // items(): IIterator<Contents.IModel> {
-  //   return new ArrayIterator(this._items);
-  // }
-
-  // selectedItems(): IIterator<Contents.IModel> {
-  //   const items = this._items;
-  //   return find(items, item => this._items[item.name]);
-  // }
 
   onAfterHide() {
     this.visibleSignal.emit(false);
@@ -49,7 +39,6 @@ export default class ListItemsWidget extends ReduxReactWidget {
   }
 
   renderReact() {
-    console.log(this.context);
     return (
       <UseSignal signal={this.visibleSignal}>
         {(_, isVisible) => (
